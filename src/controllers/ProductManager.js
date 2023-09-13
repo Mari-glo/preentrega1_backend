5290549323a31527737e32145ee2d9bdc9235fff
Mari-glo <marigloria@gmail.com>
2023-09-13 01:06:04 -0300

    segundo intento

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -16,6 +16,11 @@ class ProductManager {
 
     }
 
+    existe = async(id) => {
+        let products = await this.readProducts();
+        return products.find(prod => prod.id === id)
+    }
+
     addProducts = async (product) => {
        let productsOld = await this.readProducts();
        product.id = nanoid (3);         
@@ -29,10 +34,35 @@ class ProductManager {
     };
 
     getProductsById = async (id) => {
-        let products = await this.readProducts();
-        let productById = products.find(prod => prod.id === id)
+        let productById = await this.existe(id)
+        if(!productById) return "no se encontró el producto"
         return productById
     };
+
+    
+
+    updateProduct = async (id, product) =>{
+        let productById = await this.existe(id)
+        if(!productById) return "Producto no encontrado"
+        await this.deleteProducts(id)
+        let productsOld = await this.readProducts()
+        let products = [{...product, id : id}, ...productsOld]
+        await this.writeProducts(products)
+        return "producto actualizado correctamente"
+
+    }
+    
+    deleteProducts = async (id) =>{
+        let products = await this.readProducts();
+        let existProduct = products.some(prod => prod.id === id)
+        if (existProduct){
+            let filterProduct = products.filter (prod => prod.id != id)
+            await this. writeProducts (filterProduct)
+            return "Producto Eliminado"
+        }
+        return "El Producto que intenta eliminar  no existe"
+       
+    }
 }
 
 export default ProductManager;
--- src/index.js
+++ src/index.js
@@ -10,19 +10,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/products", async (req, res) =>{
-    res.send(await product.getProducts())
-})
+    res.send(await product.getProducts());
+});
+
 app.get("/products/:id", async (req, res) =>{
-    let id = req.params.id
-    res.send(await product.getProductsById(id))
-})
+    let id = req.params.id;
+    res.send(await product.getProductsById(id));
+});
 
 
 app.post("/products", async (req, res) =>{
-    let newProduct = req.body
-    res.send (await product.addProducts(newProduct))
+    let newProduct = req.body;
+    res.send (await product.addProducts(newProduct));
+});
 
-})
+app.put("/products/:id", async (req, res)=>{
+    let id = req.params.id;
+    let updateProduct = req.body;
+    res.send (await product.updateProduct(id, updateProduct));
+});
+
+app.delete("/products/:id", async (req, res)=>{
+    let id = req.params.id;
+    res.send (await product.deleteProducts(id));
+
+});
 
 
 app.listen(PORT, ()=>{
